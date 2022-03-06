6be3a128d404b08694c1e0c6333232fa27ce0e4e
Dmytro Malovanyy <dmitrymalovanyy@gmail.com>
2022-03-06 22:27:35 +0200

    Rewrite nmap progress bar to tqdm

--- scripts/python/nmap_with_progress.py
+++ scripts/python/nmap_with_progress.py
@@ -6,6 +6,7 @@ import subprocess
 import shlex
 import time
 import re
+from tqdm import tqdm, trange
 
 command = ""
 title = ""
@@ -22,35 +23,47 @@ def print_progress_bar(percent_float, length = 100, fill = '█', printEnd = "\r
     if filled_length == 100:
         print()
 
-
 def execute_command():
     splited_command = shlex.split(command)
     if splited_command[0] == "nmap":
-        splited_command.insert(1, "--stats-every")
-        splited_command.insert(2, "1s")
-    #print(f"Split command: {splited_command}")
+        splited_command.insert(1, "--stats-every")       
+        splited_command.insert(2, "0.4s")
     try:
         process = subprocess.Popen(splited_command,
                                     stdout=subprocess.PIPE,
                                     stderr=subprocess.PIPE)
-        current_status_message = ""
-        while True:
-            line=process.stdout.readline().decode('UTF-8')[:-1]
-            if not line:
-                break
-            if "About" in line:
-                status_message=line.split(':')[0]
-                percent=float(re.search(": About (.*)% done;", line).group(1))
-                if status_message != current_status_message:
-                    if current_status_message:
-                        print_progress_bar(float(100), prefix=current_status_message)
-                    current_status_message = status_message
-                    #Updates progress bar
-                    print_progress_bar(0, prefix=current_status_message)
-                print_progress_bar(percent, prefix=current_status_message) 
-            elif "Stats" in line:
-                continue
+        need_clear = False
+        with tqdm(total=100, ncols=140) as t:
+            current_status_message = ""
+            while True:
+                line=process.stdout.readline().decode('UTF-8')[:-1]
+                if not line:
+                    break
+                if ": About" in line:
+                    status_message=line.split(':')[0]
+                    search_result = re.search(": About (.*)% done", line)
+                    if not search_result:
+                        continue
+
+                    cur_percent=float(search_result.group(1))
+
+                    #New processing
+                    if current_status_message != status_message:
+                        if current_status_message:
+                            t.update(int(100) - t.n)
+                            print()
+                        current_status_message = status_message
+                        need_clear = True
+                        t.reset()
+                        t.set_description(desc=current_status_message, refresh=True)
+                    t.update(int(cur_percent) - t.n)
+                elif "Stats" in line:
+                    continue
+                elif need_clear:
+                    need_clear = False
+                    t.update(int(100) - t.n)
     except:
+        print ("Exception occured:", sys.exc_info())
         return
 
 if __name__ == "__main__":
